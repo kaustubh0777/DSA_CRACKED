38b03d2ae037b70f5c4fe0b12821c9c05f28ba6c
kaustubh0777 <kaustuap555@gmail.com>
2022-10-02 14:52:27 +0530

    added

---
+++ dynamic_programming/targetsum.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPartitionsUtil(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
+{
+
+    if (ind == 0)
+    {
+        if (target == 0 && arr[0] == 0)
+            return 2;
+        if (target == 0 || target == arr[0])
+            return 1;
+        return 0;
+    }
+
+    if (dp[ind][target] != -1)
+        return dp[ind][target];
+
+    int notTaken = countPartitionsUtil(ind - 1, target, arr, dp);
+
+    int taken = 0;
+    if (arr[ind] <= target)
+        taken = countPartitionsUtil(ind - 1, target - arr[ind], arr, dp);
+
+    return dp[ind][target] = (notTaken + taken);
+}
+
+int targetsum(int d, vector<int> &arr)
+{
+    int n = arr.size();
+    int totSum = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        totSum += arr[i];
+    }
+
+    // Checking for edge cases
+    if (totSum - d < 0)
+        return 0;
+    if ((totSum - d) % 2 == 1)
+        return 0;
+
+    int s2 = (totSum - d) / 2;
+
+    vector<vector<int>> dp(n, vector<int>(s2 + 1, -1));
+    return countPartitionsUtil(n - 1, s2, arr, dp);
+}
+
+int main()
+{
+    vector<int> v;
+    int in, n, d;
+    cin >> n >> d;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> in;
+        v.push_back(in);
+    }
+
+    cout << targetsum(d, v)<<endl;
+}
--- dynamic_programming/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-
-        return dp[i][w];
-    }
-
-    int nottake=0+knapsack(i-1,v,wt,w,dp);
